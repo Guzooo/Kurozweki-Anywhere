6dad7c966553fc56df1697c81de2924d060c06b7
Guzooo <guzooo-guzooo@o2.pl>
2019-10-05 07:58:11 +0200

    0.0.1
    Dodano kolory, style, Activity główne, NavigationView

---
+++ java/pl/Guzooo/KurozwekiAnywhere/MainActivity.java
@@ -0,0 +1,16 @@
+package pl.Guzooo.KurozwekiAnywhere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
