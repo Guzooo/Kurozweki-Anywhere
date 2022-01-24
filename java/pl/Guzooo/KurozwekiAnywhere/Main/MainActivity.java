33a007c59403052ab571596c8c109492d500992b
Guzooo <guzooo-guzooo@o2.pl>
2022-01-24 22:08:09 +0100

    0.2.1
    Rozpoczęcie projektu od nowa

---
+++ java/pl/Guzooo/KurozwekiAnywhere/Main/MainActivity.java
@@ -0,0 +1,16 @@
+package pl.Guzooo.KurozwekiAnywhere.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.Guzooo.KurozwekiAnywhere.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
